5ec522c15384a39536fb21e467cbab4dd0921291
MaksimMarinov <76242754+MaksimMarinov@users.noreply.github.com>
2021-01-16 16:38:34 +0200

    Delete JSAdvanced/Syntax, Functions and Statements directory

--- JSAdvanced/Syntax, Functions and Statements/Fruit.js
+++
@@ -1,4 +0,0 @@
-function buyFruit(fruitName, weight, price){
-    let moneyNeeded= weight*price/1000;
-console.log(`I need $${moneyNeeded.toFixed(2)} to buy ${(weight/1000).toFixed(2)} kilograms ${fruitName}.`);
-}buyFruit('apple', 1563, 2.35)
